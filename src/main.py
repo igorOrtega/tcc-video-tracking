e6804f6bcb8381bee37277c2f290672f85060cb4
Igor Ortega <igor.ortega@usp.br>
2020-10-01 03:55:30 -0300

    fixed tracking button, added video selection and show video option

--- src/main.py
+++ src/main.py
@@ -2,80 +2,94 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 import cv2.aruco as aruco
+import video_device_listing
 from tracking import ArucoTracking, ArucoTrackingCofig
 
-class Counter_program():
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("AR Tracking Interface")
-        self.create_widgets()
-        self.window.geometry("{}x{}+{}+{}".format(600, 300, 300, 80))            
-        self.combobox_value = tk.StringVar()
+
+class App():
+    def __init__(self, window=None):
+        self.set_window_size(window, 600, 300)
+        self.create_widgets(window)
+
+    def set_window_size(self, window, width, height):
+        pos_x = (window.winfo_screenwidth()/2) - (width/2)
+        pos_y = (window.winfo_screenheight()/2) - (height/2)
+        window.geometry('%dx%d+%d+%d' % (width, height, pos_x, pos_y))
 
     def start_tracking(self):
         config = ArucoTrackingCofig(
             2.5, aruco.DICT_6X6_250, 'localhost', 10000)
         tracking = ArucoTracking(config)
-        tracking.single_marker_tracking(0, True)
+        tracking.single_marker_tracking(
+            self.webcam_selection.current(), self.show_video.get())
 
-#_-----------------------------------------------
+# _-----------------------------------------------
     def Calibrate(self):
         return 0
-   
+
     def Capture(self):
         return 0
 # -----------------------------------------------
-    def create_widgets(self):
+
+    def create_widgets(self, window):
         # Create some room around all the internal frames
-        self.window['padx'] = 5
-        self.window['pady'] = 5
+        window['padx'] = 5
+        window['pady'] = 5
 
         # - - - - - - - - - - - - - - - - - - - - -
         # The Commands frame
-        # cmd_frame = ttk.LabelFrame(self.window, text="Commands", padx=5, pady=5, relief=tk.RIDGE)
-        cmd_frame = ttk.LabelFrame(self.window, text="AR Tracking - TCC", relief=tk.RIDGE)
-        cmd_frame.grid(row=1,column = 1, sticky = "nsew", columnspan = 2)
-        self.window.grid_rowconfigure(2, weight = 1)
+        # tracking_frame = ttk.LabelFrame(window, text="Commands", padx=5, pady=5, relief=tk.RIDGE)
+        tracking_frame = ttk.LabelFrame(
+            window, text="AR Tracking", relief=tk.RIDGE)
+        tracking_frame.grid(row=1, column=1, sticky="nsew", columnspan=2)
+        window.grid_rowconfigure(2, weight=1)
 
+        self.show_video = tk.BooleanVar()
+        self.show_video.set(True)
+        show_video_checkbox = tk.Checkbutton(
+            tracking_frame, text="Enable video", variable=self.show_video)
+        show_video_checkbox.grid(row=1, column=1)
 
-        my_button = tk.Button(cmd_frame, text="Start Tracking")
-        my_button['command']=self.start_tracking
-        my_button.grid(row=1, column=2)
+        start_tracking_button = tk.Button(
+            tracking_frame, text="Start Tracking")
+        start_tracking_button['command'] = self.start_tracking
+        start_tracking_button.grid(row=1, column=2)
 
-        combobox_label = tk.Label(cmd_frame, text="Webcams")
-        combobox_label.grid(row=2, column=1, sticky=tk.W + tk.N)
-        self.combobox_value = tk.StringVar()
-        my_combobox = ttk.Combobox(cmd_frame, height=4, textvariable=self.combobox_value)
-        my_combobox.grid(row=2, column=2)
-        my_combobox['values'] = ("web1", "web2", "web3", "web4")
-        my_combobox.current(0)
+        webcam_selection_label = tk.Label(tracking_frame, text="Video Source")
+        webcam_selection_label.grid(row=2, column=1, sticky=tk.W + tk.N)
 
-        # - - - - - - - - - - - - - - - - - - - - -
+        self.webcam_selection = ttk.Combobox(
+            tracking_frame, state="readonly", height=4, width=25)
+        self.webcam_selection.grid(row=2, column=2)
+        self.webcam_selection['values'] = video_device_listing.get_devices()
+        self.webcam_selection.current(0)
+
         # The tracking config entry frame
-        entry_frame = ttk.LabelFrame(self.window, text="Tracking Configuration",
+        entry_frame = ttk.LabelFrame(window, text="Tracking Configuration",
                                      relief=tk.RIDGE)
         entry_frame.grid(row=2, column=1, sticky=tk.E + tk.W + tk.N + tk.S)
 
-        marker_type_label = ttk.Label(entry_frame, text = "Marker Type:")
-        marker_type_label.grid(row = 1, column =1, sticky = tk.W + tk.N)
-        my_marker_type = ttk.Entry(entry_frame, width=10)
-        my_marker_type.grid(row=2, column=2, sticky=tk.W, pady=3, rowspan = 2)
+        # marker_type_label = ttk.Label(entry_frame, text="Marker Type:")
+        # marker_type_label.grid(row=1, column=1, sticky=tk.W + tk.N)
+        # my_marker_type = ttk.Entry(entry_frame, width=10)
+        # my_marker_type.grid(row=2, column=2, sticky=tk.W, pady=3, rowspan=2)
 
         marker_size_label = ttk.Label(entry_frame, text="Marker size (cm):")
         marker_size_label.grid(row=4, column=1, sticky=tk.W + tk.N)
         my_marker_size = ttk.Entry(entry_frame, width=10)
-        my_marker_size.grid(row=1, column=2, sticky=tk.W, pady=3)
-        
+        my_marker_size.grid(row=4, column=2, sticky=tk.W, pady=3)
+
         export_add_label = ttk.Label(entry_frame, text="Export address:")
         export_add_label.grid(row=7, column=1, sticky=tk.W + tk.N)
 
-        port_label = ttk.Label(entry_frame, text = "Port:")
+        export_add = ttk.Entry(entry_frame, width=15)
+        export_add.grid(row=7, column=2, sticky=tk.W + tk.N)
+
+        port_label = ttk.Label(entry_frame, text="Port:")
         port_label.grid(row=7, column=3, sticky=tk.W + tk.N)
 
-        port_label_add = ttk.Entry(entry_frame, width = 5)
-        port_label_add.grid(row=7, column = 4, sticky = tk.W + tk.N)
-
-
+        port_label_add = ttk.Entry(entry_frame, width=5)
+        port_label_add.grid(row=7, column=4, sticky=tk.W + tk.N)
 
         #my_text = tk.Text(entry_frame, height=5, width=30)
         #my_text.grid(row=2, column=2)
@@ -83,59 +97,69 @@ class Counter_program():
 
     # - - - - - - - - - - - - - - - - - - - - -
         # The calibration config frame
-        calib_config_frame = ttk.LabelFrame(self.window, text="Calibration Configuration", relief=tk.RIDGE, padding=6)
-        calib_config_frame.grid(row=2, column=3, padx=6, sticky=tk.E + tk.W + tk.N + tk.S, columnspan = 2)
+        calib_config_frame = ttk.LabelFrame(
+            window, text="Calibration Configuration", relief=tk.RIDGE, padding=6)
+        calib_config_frame.grid(row=2, column=3, padx=6,
+                                sticky=tk.E + tk.W + tk.N + tk.S, columnspan=2)
 
-        chess_label = ttk.Label(calib_config_frame, text="Chessboard square size (cm):")
+        chess_label = ttk.Label(
+            calib_config_frame, text="Chessboard square size (cm):")
         chess_label.grid(row=1, rowspan=2, column=1, sticky=tk.W + tk.N)
-        chess_label_add = ttk.Entry(calib_config_frame, width = 5)
-        chess_label_add.grid(row=1, column = 3)
+        chess_label_add = ttk.Entry(calib_config_frame, width=5)
+        chess_label_add.grid(row=1, column=3)
 
-        chess_column_label = ttk.Label(calib_config_frame, text="Chessboard column count:")
+        chess_column_label = ttk.Label(
+            calib_config_frame, text="Chessboard column count:")
         chess_column_label.grid(row=3, rowspan=2, column=1, sticky=tk.W + tk.N)
-        chess_column_add = ttk.Entry(calib_config_frame, width = 5)
-        chess_column_add.grid(row=3, column = 3)
+        chess_column_add = ttk.Entry(calib_config_frame, width=5)
+        chess_column_add.grid(row=3, column=3)
 
-        chess_row_label = ttk.Label(calib_config_frame, text="Chessboard row count:")
+        chess_row_label = ttk.Label(
+            calib_config_frame, text="Chessboard row count:")
         chess_row_label.grid(row=5, rowspan=2, column=1, sticky=tk.W + tk.N)
-        chess_row_add = ttk.Entry(calib_config_frame, width = 5)
-        chess_row_add.grid(row=5, column = 3)
+        chess_row_add = ttk.Entry(calib_config_frame, width=5)
+        chess_row_add.grid(row=5, column=3)
 
-        calib_img_count = ttk.Label(calib_config_frame, text = "Calibration image count:")
-        calib_img_count.grid(row = 8, column = 1, rowspan = 3,  sticky=tk.W)
+        calib_img_count = ttk.Label(
+            calib_config_frame, text="Calibration image count:")
+        calib_img_count.grid(row=8, column=1, rowspan=3,  sticky=tk.W)
 
-
-
-        #aqui
+        # aqui
         calibrate_b = tk.Button(calib_config_frame, text="Calibrate")
         calibrate_b['command'] = self.Calibrate
         calibrate_b.grid(row=11, column=1)
 
-        #aqui 2
-        capture_img_b = tk.Button(calib_config_frame, text= "Capture Images")
+        # aqui 2
+        capture_img_b = tk.Button(calib_config_frame, text="Capture Images")
         capture_img_b['command'] = self.Capture
-        capture_img_b.grid(row = 12, column = 1)
+        capture_img_b.grid(row=12, column=1)
 
-        menubar = tk.Menu(self.window)
+        menubar = tk.Menu(window)
 
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open", command=filedialog.askopenfilename)
-        filemenu.add_command(label="Save", command=filedialog.asksaveasfilename)
+        filemenu.add_command(
+            label="Save", command=filedialog.asksaveasfilename)
         filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=self.window.quit)
+        filemenu.add_command(label="Exit", command=window.quit)
         menubar.add_cascade(label="File", menu=filemenu)
 
-        self.window.config(menu=menubar)
+        window.config(menu=menubar)
 
         # - - - - - - - - - - - - - - - - - - - - -
-        save_button = ttk.Button(self.window, text ="Salvar", command = filedialog.asksaveasfilename)
-        save_button.grid(row = 1, column = 3)
+        save_button = ttk.Button(
+            window, text="Salvar", command=filedialog.asksaveasfilename)
+        save_button.grid(row=1, column=3)
         # Quit button in the lower right corner
-        quit_button = ttk.Button(self.window, text="Sair", command=self.window.destroy)
+        quit_button = ttk.Button(
+            window, text="Sair", command=window.destroy)
         quit_button.grid(row=2, column=5)
-        
-# Create the entire GUI program
-program = Counter_program()
 
-# Start the GUI event loop
-program.window.mainloop()
+
+if __name__ == "__main__":
+    tk_root = tk.Tk("AR Tracking Interface")
+    # Create the entire GUI program
+    App(tk_root)
+
+    # Start the GUI event loop
+    tk_root.mainloop()
