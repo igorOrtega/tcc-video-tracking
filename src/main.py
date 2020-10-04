e8002da615d53c47fe233eb27234f60145ec3bd6
igorOrtega <48002130+igorOrtega@users.noreply.github.com>
2020-10-04 11:53:00 -0300

    ui design

--- src/main.py
+++ src/main.py
@@ -21,12 +21,16 @@ class App():
         pos_x = (window.winfo_screenwidth()/2) - (width/2)
         pos_y = (window.winfo_screenheight()/2) - (height/2)
         window.geometry('%dx%d+%d+%d' % (width, height, pos_x, pos_y))
+        window.resizable(0, 0)
 
         # Create some room around all the internal frames
         window['padx'] = 5
         window['pady'] = 5
 
-        window.grid_rowconfigure(0, weight=0)
+        window.grid_rowconfigure(1, weight=1)
+        window.grid_rowconfigure(2, weight=1)
+        window.grid_rowconfigure(3, weight=1)
+        window.grid_rowconfigure(4, weight=1)
         window.grid_columnconfigure(1, weight=1)
 
         self.video_source_frame = tk.Frame(window)
@@ -46,7 +50,7 @@ class App():
         self.refresh_video_sources_button = tk.Button(
             self.video_source_frame, text="Refresh")
         self.refresh_video_sources_button['command'] = self.refresh_video_sources
-        self.refresh_video_sources_button.grid(row=1, column=3)
+        self.refresh_video_sources_button.grid(row=1, column=3, padx=5)
 
         self.tracking_commands_frame = tk.Frame(window)
         self.tracking_commands_frame.grid(row=2, column=1)
@@ -57,89 +61,120 @@ class App():
         #     self.webcam_selection.current(), self.show_video.get())
         self.start_tracking_button.grid(row=1, column=1)
 
-    #     tracking_frame = ttk.LabelFrame(
-    #         window, text="AR Tracking", relief=tk.RIDGE)
-    #     tracking_frame.grid(row=1, column=1, sticky="nsew", columnspan=2)
-    #     window.grid_rowconfigure(2, weight=1)
+        self.configuration_frame = tk.Frame(window)
+        self.configuration_frame.grid(
+            row=3, column=1, sticky=tk.E + tk.W + tk.N + tk.S)
 
-    #     self.show_video = tk.BooleanVar()
-    #     self.show_video.set(True)
-    #     show_video_checkbox = tk.Checkbutton(
-    #         tracking_frame, text="Enable video", variable=self.show_video)
-    #     show_video_checkbox.grid(row=1, column=1)
+        self.configuration_frame.grid_columnconfigure(1, weight=1)
+        self.configuration_frame.grid_columnconfigure(2, weight=1)
 
-    #     # The tracking config entry frame
-    #     entry_frame = ttk.LabelFrame(window, text="Tracking Configuration",
-    #                                  relief=tk.RIDGE)
-    #     entry_frame.grid(row=2, column=1, sticky=tk.E + tk.W + tk.N + tk.S)
+        self.tracking_config_frame = ttk.LabelFrame(
+            self.configuration_frame, text="Tracking Configuration")
+        self.tracking_config_frame.grid(
+            row=1, column=1, sticky=tk.E + tk.W + tk.N + tk.S, pady=5, padx=5)
 
-    #     marker_size_label = ttk.Label(entry_frame, text="Marker size (cm):")
-    #     marker_size_label.grid(row=4, column=1, sticky=tk.W + tk.N)
-    #     my_marker_size = ttk.Entry(entry_frame, width=10)
-    #     my_marker_size.grid(row=4, column=2, sticky=tk.W, pady=3)
+        self.tracking_config_frame.grid_columnconfigure(1, weight=1)
 
-    #     export_add_label = ttk.Label(entry_frame, text="Export address:")
-    #     export_add_label.grid(row=7, column=1, sticky=tk.W + tk.N)
+        self.show_video_frame = tk.Frame(self.tracking_config_frame)
+        self.show_video_frame.grid(row=1, column=1)
+        self.show_video = tk.BooleanVar()
+        self.show_video.set(True)
+        self.show_video_checkbox = tk.Checkbutton(
+            self.show_video_frame, text="Show video", variable=self.show_video)
+        self.show_video_checkbox.grid(row=1, column=1, pady=5)
 
-    #     export_add = ttk.Entry(entry_frame, width=15)
-    #     export_add.grid(row=7, column=2, sticky=tk.W + tk.N)
+        self.marker_parameters_frame = tk.Frame(self.tracking_config_frame)
+        self.marker_parameters_frame.grid(row=2, column=1, pady=5)
+        self.marker_side_lenght_label = ttk.Label(
+            self.marker_parameters_frame, text="Marker side lenght (cm):")
+        self.marker_side_lenght_label.grid(
+            row=1, column=1, sticky=tk.W + tk.N)
+        self.marker_side_lenght = ttk.Entry(
+            self.marker_parameters_frame, width=10)
+        self.marker_side_lenght.grid(row=1, column=2, sticky=tk.W)
 
-    #     port_label = ttk.Label(entry_frame, text="Port:")
-    #     port_label.grid(row=7, column=3, sticky=tk.W + tk.N)
+        self.export_coordinates_frame = ttk.LabelFrame(
+            self.tracking_config_frame, text="Coordinates Publish Server")
+        self.export_coordinates_frame.grid(row=3, column=1, pady=5)
 
-    #     port_label_add = ttk.Entry(entry_frame, width=7)
-    #     port_label_add.grid(row=7, column=4, sticky=tk.W + tk.N)
+        self.export_ip_label = ttk.Label(
+            self.export_coordinates_frame, text="IP Address:")
+        self.export_ip_label.grid(row=1, column=1)
 
-    # # - - - - - - - - - - - - - - - - - - - - -
-    #     # The calibration config frame
-    #     calib_config_frame = ttk.LabelFrame(
-    #         window, text="Calibration Configuration", relief=tk.RIDGE, padding=6)
-    #     calib_config_frame.grid(row=2, column=3, padx=6,
-    #                             sticky=tk.E + tk.W + tk.N + tk.S, columnspan=2)
+        self.export_ip = ttk.Entry(self.export_coordinates_frame, width=15)
+        self.export_ip.grid(row=1, column=2)
 
-    #     chess_label = ttk.Label(
-    #         calib_config_frame, text="Chessboard square size (cm):")
-    #     chess_label.grid(row=1, rowspan=2, column=1, sticky=tk.W + tk.N)
-    #     chess_label_add = ttk.Entry(calib_config_frame, width=5)
-    #     chess_label_add.grid(row=1, column=3)
+        self.export_port_label = ttk.Label(
+            self.export_coordinates_frame, text="Port:")
+        self.export_port_label.grid(row=1, column=3)
 
-    #     chess_column_label = ttk.Label(
-    #         calib_config_frame, text="Chessboard column count:")
-    #     chess_column_label.grid(row=3, rowspan=2, column=1, sticky=tk.W + tk.N)
-    #     chess_column_add = ttk.Entry(calib_config_frame, width=5)
-    #     chess_column_add.grid(row=3, column=3)
+        self.export_port = ttk.Entry(self.export_coordinates_frame, width=7)
+        self.export_port.grid(row=1, column=4)
 
-    #     chess_row_label = ttk.Label(
-    #         calib_config_frame, text="Chessboard row count:")
-    #     chess_row_label.grid(row=5, rowspan=2, column=1, sticky=tk.W + tk.N)
-    #     chess_row_add = ttk.Entry(calib_config_frame, width=5)
-    #     chess_row_add.grid(row=5, column=3)
+        self.calibration_config_frame = ttk.LabelFrame(
+            self.configuration_frame, text="Calibration")
+        self.calibration_config_frame.grid(
+            row=1, column=2, sticky=tk.E + tk.W + tk.N + tk.S, pady=5, padx=5)
 
-    #     calib_img_count = ttk.Label(
-    #         calib_config_frame, text="Calibration image count:")
-    #     calib_img_count.grid(row=8, column=1, rowspan=3,  sticky=tk.W)
+        self.calibration_config_frame.grid_columnconfigure(1, weight=1)
 
-    #     cam = CameraCalibration(CalibrationCofig)
-    #     img_count = ttk.Label(calib_config_frame,
-    #                           text=cam.calibration_image_count)
-    #     img_count.grid(row=8, column=2)
+        self.calibration_frame = tk.Frame(self.calibration_config_frame)
+        self.calibration_frame.grid(row=1, column=1)
+        self.calibrate_button = tk.Button(
+            self.calibration_frame, text="Calibrate")
+        # self.calibrate_button['command'] = lambda: self.Calibrate()
+        self.calibrate_button.grid(row=1, column=1, padx=5)
+        self.capture_images_button = tk.Button(
+            self.calibration_frame, text="Capture Images")
+        # self.capture_images_button['command'] = lambda: self.Capture()
+        self.capture_images_button.grid(row=1, column=2, padx=5)
 
-    #     calibrate_b = tk.Button(calib_config_frame, text="Calibrate")
-    #     calibrate_b['command'] = lambda: self.Calibrate()
-    #     calibrate_b.grid(row=11, column=1)
+        self.calibration_image_count_text = tk.StringVar()
+        self.calibration_image_count_text.set('Calibration images count: 0')
+        self.calibration_image_count_label = ttk.Label(
+            self.calibration_config_frame, textvariable=self.calibration_image_count_text)
+        self.calibration_image_count_label.grid(row=2, column=1)
 
-    #     capture_img_b = tk.Button(calib_config_frame, text="Capture Images")
-    #     capture_img_b['command'] = lambda: self.Capture()
-    #     capture_img_b.grid(row=12, column=1)
+        self.calibration_chessboard_parameters_frame = tk.Frame(
+            self.calibration_config_frame)
+        self.calibration_chessboard_parameters_frame.grid(
+            row=3, column=1, pady=5)
 
-    #     # - - - - - - - - - - - - - - - - - - - - -
-    #     save_button = ttk.Button(
-    #         window, text="Salvar", command=filedialog.asksaveasfilename)
-    #     save_button.grid(row=1, column=3)
-    #     # Quit button in the lower right corner
-    #     quit_button = ttk.Button(
-    #         window, text="Sair", command=window.destroy)
-    #     quit_button.grid(row=2, column=5)
+        self.calibration_chessboard_square_size_label = ttk.Label(
+            self.calibration_chessboard_parameters_frame, text="Chessboard square size (cm):")
+        self.calibration_chessboard_square_size_label.grid(
+            row=1, column=1)
+
+        self.calibration_chessboard_square_size = ttk.Entry(
+            self.calibration_chessboard_parameters_frame, width=5)
+        self.calibration_chessboard_square_size.grid(row=1, column=2)
+
+        self.calibration_chessboard_rows_label = ttk.Label(
+            self.calibration_chessboard_parameters_frame, text="Chessboard row count:")
+        self.calibration_chessboard_rows_label.grid(row=2, column=1)
+
+        self.calibration_chessboard_rows = ttk.Entry(
+            self.calibration_chessboard_parameters_frame, width=5)
+        self.calibration_chessboard_rows.grid(row=2, column=2)
+
+        self.calibration_chessboard_cols_label = ttk.Label(
+            self.calibration_chessboard_parameters_frame, text="Chessboard column count:")
+        self.calibration_chessboard_cols_label.grid(row=3, column=1)
+
+        self.calibration_chessboard_cols = ttk.Entry(
+            self.calibration_chessboard_parameters_frame, width=5)
+        self.calibration_chessboard_cols.grid(row=3, column=2)
+
+        self.utils_frame = tk.Frame(window)
+        self.utils_frame.grid(row=4, column=1, sticky=tk.S)
+
+        self.save_button = ttk.Button(
+            self.utils_frame, text="Save")
+        self.save_button.grid(row=1, column=1, padx=5)
+
+        self.quit_button = ttk.Button(
+            self.utils_frame, text="Exit", command=window.destroy)
+        self.quit_button.grid(row=1, column=2, padx=5)
 
     # def Calibrate(self):
     #     config = CalibrationCofig(5, 10, 2.5)
@@ -155,6 +190,7 @@ class App():
 
     def refresh_video_sources(self):
         self.video_source_list = video_device_listing.get_devices()
+        self.video_source['values'] = self.video_source_list
 
 
 if __name__ == "__main__":
