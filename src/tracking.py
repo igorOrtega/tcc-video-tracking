78562935aabaeeedc43fb34ce2221ff8614353f1
igorOrtega <48002130+igorOrtega@users.noreply.github.com>
2020-11-26 00:41:25 -0300

    melhoria de estabilidade marker

--- src/tracking.py
+++ src/tracking.py
@@ -176,7 +176,8 @@ class Tracking:
 
     def __detect_markers(self, frame):
         parameters = aruco.DetectorParameters_create()
-        parameters.adaptiveThreshConstant = 10
+        parameters.adaptiveThreshConstant = 7
+        parameters.cornerRefinementMethod = aruco.CORNER_REFINE_CONTOUR
 
         corners, ids, _ = aruco.detectMarkers(
             cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),
